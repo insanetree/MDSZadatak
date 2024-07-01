6873d9f644e22d29234656346a59a8dbdb6bc9e5
Aleksandar <pa200059d@student.etf.bg.ac.rs>
2024-07-01 12:48:33 +0200

    Dodat je host  parametar za pokretanje aplikacije. Izbacen jsonify gde ne treba

--- endpoints/zaposleni.py
+++ endpoints/zaposleni.py
@@ -24,7 +24,7 @@ def avg_salary():
 	result = float(result[0])
 	cursor.close() 
 	connection.close()
-	return jsonify({"avg_salary": result}), 200
+	return {"avg_salary": result}, 200
 
 @zaposleni_bp.route("/third_highest_salary", methods=["GET"])
 def third_highest_salary():
@@ -35,17 +35,17 @@ def third_highest_salary():
 	result = cursor.fetchone()
 	cursor.close() 
 	connection.close()
-	return jsonify({"third_highest": result[0]}), 200
+	return {"third_highest": result[0]}, 200
 
 @zaposleni_bp.route("/like_a")
 def like_a():
 	connection = get_db_connection()
 	cursor = connection.cursor()
-	query = "SELECT firstname, lastname, username, email FROM employee where firstname LIKE \"a%\""
+	query = "SELECT firstname, lastname, username, email, id, salary FROM employee where firstname LIKE \"a%\""
 	cursor.execute(query)
 	result = cursor.fetchall()
 	list = []
 	for row in result:
-		e = {"firstname": row[0], "lastname": row[1], "username": row[2], "email": row[3]}
+		e = {"firstname": row[0], "lastname": row[1], "username": row[2], "email": row[3], "id":int(row[4]), "salary": int(row[5])}
 		list.append(e)
 	return list, 200
